889631d13b8aa5a4f7fee825df271d123be5c81f
Laza Bogdan <laza.bogdan@macro-net.ro>
2016-01-03 04:57:47 +0200

    update docs

--- resources/docs/src/components/sidebar-toggle-button.vue
+++ resources/docs/src/components/sidebar-toggle-button.vue
@@ -3,7 +3,18 @@
 	import merge from 'mout/object/merge'
 
 	let docs = merge(component, {
-		description: `Create a button to toggle the visibility for the [sidebar](#!/components/sidebar) and [sidebar-transition](#!/components/sidebar-transition) components.`
+		description: `Create a button to toggle the visibility for the [sidebar](#!/components/sidebar) and [sidebar-transition](#!/components/sidebar-transition) components.`,
+		props: {
+			sidebarId: {
+				description: `Must match the \`sidebar-id\` property value of the corresponding [sidebar](#!/components/sidebar) or [sidebar-transition](#!/components/sidebar-transition) component.`
+			},
+			icon: {
+				description: `Provide a class name to add a button icon.`
+			},
+			label: {
+				description: `Provide the text for the button.`
+			}
+		}
 	})
 
 	export default docs
--- resources/docs/src/components/sidebar.vue
+++ resources/docs/src/components/sidebar.vue
@@ -27,13 +27,62 @@
 		`,
 		props: {
 			scrollable: {
-				description: 'Make the sidebar content area scrollable.'
+				description: `Make the sidebar content area scrollable.`
 			},
 			position: {
-				description: 'Position the sidebar left or right.'
+				description: `Position the sidebar left or right.`
 			},
 			size: {
-				description: 'Set the sidebar width to a fixed predefined size.'
+				description: `
+					Set the sidebar width to a fixed predefined size. The value should be \`1\` for \`56px\`, \`2\` for \`200px\` and \`3\` for \`250px\`.
+				`
+			},
+			sizeXs: {
+				description: `Set the sidebar width for screens up to \`768px\`.`
+			},
+			sizeSm: {
+				description: `Set the sidebar width for screens between \`768px\` and \`992px\`.`
+			},
+			sizeMd: {
+				description: `Set the sidebar width for screens **>=** than \`992px\`.`
+			},
+			sizeLg: {
+				description: `Set the sidebar width for screens **>=** than \`1200px\`.`
+			},
+			visible: {
+				description: `
+					Enable the sidebar visibility on any screen size, on specific screen sizes or screen size range. Accepts the following values **in any combination**: \`xs\`, \`sm\`, \`md\`, \`lg\` and \`xs-up\`, \`sm-up\`, \`md-up\`, \`lg-up\`
+
+					##### default: \`sm-up\`
+				`
+			},
+			offset: {
+				description: `
+					Offset or move the sidebar to the opposite direction of it's position, in order to make room for another sidebar element on the same side. Only required when using more than 1 sidebar on the same side.
+
+					#### Offset Example
+
+					##### Scenario:
+
+					- we have 2 sidebar elements on the page, sidebar A and sidebar B
+					- sidebar A has the size property value of \`1\` (\`56px\`) and the \`sidebar-id\` property value of \`sidebar-A\`
+					- sidebar B has the size property value of \`2\` (\`200px\`)
+					- both sidebars are positioned on the left
+					- we want sidebar B to be placed next to sidebar A, to the right
+
+					##### Usage:
+
+					- sidebar B will have the offset property value of \`sidebar-A\`, which is the \`sidebar-id\` property value of sidebar A
+				`
+			},
+			toggleBar: {
+				description: `Display a vertical toggle bar along the sidebar element.`
+			},
+			sidebarId: {
+				description: `Assign a unique identifier for the sidebar element.`
+			},
+			show: {
+				description: `Control the visibility of the sidebar element on the initial page load.`
 			}
 		}
 	})
