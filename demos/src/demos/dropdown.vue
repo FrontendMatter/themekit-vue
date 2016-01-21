189ca03559843e9bdefb9b09f2d1e086b33d689d
AdrianDemian <adrian.demian@mosaicpro.biz>
2016-01-21 12:41:42 +0200

    add dropdown, dropzone, expandable, modal

---
+++ demos/src/demos/dropdown.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="container-fluid">
+		<h1>Dropdowns</h1>
+		<dropdown-area label="Dropdown" 
+		btn-class="btn btn-default"
+		class="dropdown">
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda eligendi possimus placeat fugiat quia provident culpa illo quos natus.</p>
+		</dropdown-area>
+
+		<hr>
+		<dropdown-area label="Settings" 
+		icon="fa fa-fw fa-cog" 
+		btn-class="btn btn-primary"
+		class="dropdown">
+			<dropdown-menu 
+				v-for="menu in menus"
+				:model="menu">
+			</dropdown-menu>
+		</dropdown-area>
+			
+	</div>
+</template>
+
+<script>
+	import { Dropdown } from 'themekit-vue'
+	import { DropdownArea } from 'themekit-vue'
+	import { DropdownMenu } from 'themekit-vue'
+	
+	export default {
+		data () {
+			return {
+				menus: [
+					{ label: 'Dashboard', icon: 'fa fa-home' },
+					{ label: 'Email', icon: 'fa fa-envelope' },
+					{ label: 'Slack', icon: 'fa fa-group' },
+					{ label: 'Chat', icon: 'fa fa-comments' },
+					{ label: 'Todo', icon: 'fa fa-list' },
+					{ label: 'Notes', icon: 'fa fa-pencil' },
+					{ label: 'Charts', icon: 'fa fa-bar-chart-o' }
+				]
+			}
+		},
+		components: {
+			Dropdown,
+			DropdownArea,
+			DropdownMenu
+		}
+	}
+</script>
---
+++ demos/src/demos/expandable.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="container-fluid">
+		<!-- Isotope Layout -->
+		<isotope>
+
+			<!-- Grid Column -->
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h4 class="page-section-heading">Example #1 (Small preview)</h4>
+
+				<expandable>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi delectus distinctio eaque, esse, id maxime modi, nam neque nihil qui quia quibusdam quis quisquam similique sunt totam veritatis. Ad autem consequatur cum delectus deleniti dolor dolores eius, eligendi error id nesciunt odit officia perferendis quasi recusandae similique tempora tenetur voluptas.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dignissimos maiores nisi optio quas, quod? Animi fuga inventore laborum nemo quaerat quis quod? Alias, architecto consequuntur corporis deleniti dolor facilis id minima necessitatibus nihil perspiciatis quo repellendus saepe, sunt suscipit velit. Deleniti dolorum id maxime omnis perspiciatis sed, veniam?</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam autem doloribus iste nesciunt nihil nisi odio, optio temporibus. Alias dolore eveniet expedita neque nesciunt nihil qui reprehenderit repudiandae tempore? A consectetur ipsam magni quibusdam totam? At earum id incidunt iure labore libero, perferendis perspiciatis, saepe sed suscipit velit veritatis.</p>
+				</expandable>
+
+			</div>
+			<!-- // END Grid Column -->
+
+			<!-- Grid Column -->
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h4 class="page-section-heading">Example #2 (Large preview)</h4>
+
+				<expandable content-size="large">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi delectus distinctio eaque, esse, id maxime modi, nam neque nihil qui quia quibusdam quis quisquam similique sunt totam veritatis. Ad autem consequatur cum delectus deleniti dolor dolores eius, eligendi error id nesciunt odit officia perferendis quasi recusandae similique tempora tenetur voluptas.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dignissimos maiores nisi optio quas, quod? Animi fuga inventore laborum nemo quaerat quis quod? Alias, architecto consequuntur corporis deleniti dolor facilis id minima necessitatibus nihil perspiciatis quo repellendus saepe, sunt suscipit velit. Deleniti dolorum id maxime omnis perspiciatis sed, veniam?</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam autem doloribus iste nesciunt nihil nisi odio, optio temporibus. Alias dolore eveniet expedita neque nesciunt nihil qui reprehenderit repudiandae tempore? A consectetur ipsam magni quibusdam totam? At earum id incidunt iure labore libero, perferendis perspiciatis, saepe sed suscipit velit veritatis.</p>
+				</expandable>
+
+			</div>
+			<!-- // END Grid Column -->
+
+			<!-- Grid Column -->
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h4 class="page-section-heading">Example #3 (Expandable on white background)</h4>
+
+				<expandable-panel indicator-color="white">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi delectus distinctio eaque, esse, id maxime modi, nam neque nihil qui quia quibusdam quis quisquam similique sunt totam veritatis. Ad autem consequatur cum delectus deleniti dolor dolores eius, eligendi error id nesciunt odit officia perferendis quasi recusandae similique tempora tenetur voluptas.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dignissimos maiores nisi optio quas, quod? Animi fuga inventore laborum nemo quaerat quis quod? Alias, architecto consequuntur corporis deleniti dolor facilis id minima necessitatibus nihil perspiciatis quo repellendus saepe, sunt suscipit velit. Deleniti dolorum id maxime omnis perspiciatis sed, veniam?</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam autem doloribus iste nesciunt nihil nisi odio, optio temporibus. Alias dolore eveniet expedita neque nesciunt nihil qui reprehenderit repudiandae tempore? A consectetur ipsam magni quibusdam totam? At earum id incidunt iure labore libero, perferendis perspiciatis, saepe sed suscipit velit veritatis.</p>
+				</expandable-panel>
+
+			</div>
+			<!-- // END Grid Column -->
+
+			<!-- Grid Column -->
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h4 class="page-section-heading">Example #4 (Expandable large on white background)</h4>
+				
+				<expandable-panel indicator-color="white" content-size="large">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi delectus distinctio eaque, esse, id maxime modi, nam neque nihil qui quia quibusdam quis quisquam similique sunt totam veritatis. Ad autem consequatur cum delectus deleniti dolor dolores eius, eligendi error id nesciunt odit officia perferendis quasi recusandae similique tempora tenetur voluptas.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dignissimos maiores nisi optio quas, quod? Animi fuga inventore laborum nemo quaerat quis quod? Alias, architecto consequuntur corporis deleniti dolor facilis id minima necessitatibus nihil perspiciatis quo repellendus saepe, sunt suscipit velit. Deleniti dolorum id maxime omnis perspiciatis sed, veniam?</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam autem doloribus iste nesciunt nihil nisi odio, optio temporibus. Alias dolore eveniet expedita neque nesciunt nihil qui reprehenderit repudiandae tempore? A consectetur ipsam magni quibusdam totam? At earum id incidunt iure labore libero, perferendis perspiciatis, saepe sed suscipit velit veritatis.</p>
+				</expandable-panel>
+
+			</div>
+			<!-- // END Grid Column -->
+
+		</isotope>
+		<!-- // END Isotope Layout-->
+
+	</div>
+</template>
+
+<script>
+	import { Expandable } from 'themekit-vue'
+	import { ExpandablePanel } from 'themekit-vue'
+	import { Isotope } from 'themekit-vue'
+
+	export default {
+		components: {
+			Expandable,
+			ExpandablePanel,
+			Isotope
+		}
+	}
+</script>
